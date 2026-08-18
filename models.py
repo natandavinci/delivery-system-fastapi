3aec7f94d7e0b7ec38dd502499cf1bb16a4fe860
natandavinci <natanaelqueiroz969@gmail.com>
2026-08-18 06:58:42 -0300

    add: tabela pedidos e itens_pedido

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
 from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
 
 # Conection
 db = create_engine("sqlite:///banco.db")
@@ -23,6 +24,44 @@ class Usuario(Base):
         self.email = email
         self.ativo = ativo
         self.admin = admin
+
+class Pedido(Base):
+    __tablename__ = "pedidos"
+
+    "STATUS_PEDIDO" = (
+        ("PENDENTE", "PENDENTE"),
+        ("CANCELADO","CANCELADO"),
+        ("FINALIZADO", "FINALIZADO")
+    )
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", ChoiceType(choices="STATUS_PEDIDO"))
+    usuario = Column("usuario", ForeignKey("usuarios.id"))
+    preco = Column("preco", Float)
+
+    def __int__(self,status,usuario,preco):
+        self.status = status
+        self.usuario = usuario
+        self.preco = preco
+
+class itemPedido(Base):
+    __tablename__ = "itensPedido"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantidade = Column("quantidade", Integer)
+    sabor = Column("sabor", String)
+    preco_unitario = Column("preco_unitario", Float)
+    pedido = Column("pedido", ForeignKey("pedidos.id"))
+
+    def __int__(self, quantidade, sabor, preco_unitario, pedido):
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.preco_unitario = preco_unitario
+        self.pedido = pedido
+        
+
+        
+
 
 
 
